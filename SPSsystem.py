544e1dd6b2e904c23d47b6ff968743af190f5150
Itsuki Jozuka <itsuki20110218@icloud.com>
2025-12-15 16:51:43 +0900

    SPS-3

--- SPSsystem.py
+++ SPSsystem.py
@@ -1,1 +1,15 @@
-print("hello SPS")
+import schedule
+import time
+import datetime
+
+def main():
+    now = datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
+    print(now, flush=True)
+
+if __name__ == "__main__":
+    main()
+    # 1分毎に実行
+    schedule.every(1).minutes.do(main)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
