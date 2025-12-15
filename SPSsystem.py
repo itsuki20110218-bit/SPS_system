c72426e701ef92d2a04ddd890580df627fba0d14
Itsuki Jozuka <itsuki20110218@icloud.com>
2025-12-15 18:30:02 +0900

    起動確認

--- SPSsystem.py
+++ SPSsystem.py
@@ -1,15 +1,9 @@
-import schedule
-import time
-import datetime
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/callback", methods=["POST"])
+def callback():
+    print(request.get_json())
+    return "OK"
 
-def main():
-    now = datetime.datetime.now().strftime('%Y年%m月%d日 %H:%M:%S')
-    print(now, flush=True)
-
-if __name__ == "__main__":
-    main()
-    # 1分毎に実行
-    schedule.every(1).minutes.do(main)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+if __name__ = "__main__":
+    app.run()
