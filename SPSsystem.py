98fe29651d9ed2f5915b276d1084177092ee6975
Itsuki Jozuka <itsuki20110218@icloud.com>
2025-12-17 15:53:51 +0900

    接続確認

--- SPSsystem.py
+++ SPSsystem.py
@@ -2,8 +2,5 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route("/callback", methods=["POST"])
 def callback():
-    print(request.get_json())
-    return "OK"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    print(request.get_json(), flush=True)
+    return "OK", 200
