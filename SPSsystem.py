dfd8df1f80ef66b0601ab30d0f0d301341276742
Itsuki <itsuki20110218@icloud.com>
2025-12-22 23:11:06 +0900

    バグ修正・ユーザー情報登録機能追加

--- SPSsystem.py
+++ SPSsystem.py
@@ -8,8 +8,6 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = "KR7Sclg6pbBPdSFHkwyz3czQpCKOzP6ppszkWFROU8kvM0QdV7XaQ6A7bqDOX27qiZCxBCLA6VWa+Ke85Ekni+Fxwi7vasS9dz4+q5KRVfbIN2uhF2XCSrLaJlsOeQAsnQDUE6O7tFyEZemn72DccAdB04t89/1O/w1cDnyilFU="
 USER_FILE = "users.json"
 
-@app.route("/callback", methods=["POST"])
-
 def load_users():
     if not os.path.exists(USER_FILE): 
         return {}
@@ -26,6 +24,7 @@ def add_users(user_id):
     with open(USER_FILE, "w", encoding= "utf-8") as f:
         json.dump(users, f, ensure_ascii= False, indent= 2)
 
+@app.route("/callback", methods=["POST"])
 def callback():
     data = request.get_json()
     print(data, flush=True)  # Webhook届いたか確認用
@@ -39,10 +38,10 @@ def callback():
         if user_id not in users:
             reply_message(reply_token, "adding user infomation")
             add_users(user_id)
-            return("OK")
+            return "OK"
         else:
             reply_message(reply_token, "auto_reply run successfully")
-            return("OK")
+            return "OK"
 
 def reply_message(reply_token, text):
     url = "https://api.line.me/v2/bot/message/reply"
