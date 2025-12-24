7acec3a9222b30e8e552b161a61c04dc28115cc4
Itsuki <itsuki20110218@icloud.com>
2025-12-24 22:34:11 +0900

    Update SPSsystem.py

--- SPSsystem.py
+++ SPSsystem.py
@@ -18,9 +18,10 @@ def load_users():
 def add_users(user_id): # ユーザー情報を追加
     users = load_users()
     users[user_id] = {
-        "status": "waiting_name",
+        "register_status": "waiting_name",
         "name": "unknown",
-        "class": "unknown"
+        "class": "unknown",
+        "service_status": "None"
     }
     with open(USER_FILE, "w", encoding= "utf-8") as f:
         json.dump(users, f, ensure_ascii= False, indent= 2)
@@ -37,9 +38,9 @@ def callback():
     for event in data.get("events", []):
 
         if event["type"] != "message" or event["message"]["type"] != "text":
-            return "OK"
+            continue
         
-        reply_token = event["replyToken"]
+        reply_token = event["replyToken"]   # ローカル関数にしてるのはeventが来た時に使うから
         user_id = event["source"]["userId"]
         users = load_users()
         text = event["message"]["text"]
@@ -47,27 +48,54 @@ def callback():
         if user_id not in users:
             reply_message(reply_token, "はじめまして。初回利用のため、ユーザー情報を登録します。\n本名を入力してください。")
             add_users(user_id)
-            continue
+            return "OK"
         
         else:
-            status = users[user_id]["status"]
+            register_status = users[user_id]["register_status"]
+            service_status = users[user_id]["service_status"]
 
-            if status == "waiting_name":
-                reply_message(reply_token, "本名を登録しました。次にクラスを入力してください。")
-                users[user_id]["status"] = "waiting_class"
-                users[user_id]["name"] = text
-                save_users(users)
-                return "OK"
+            if register_status == "waiting_name":
+                if text == "サービスを利用":
+                    reply_message(reply_token, "本名を入力してください。")
+                    return "OK"
+                else:
+                    reply_message(reply_token, "本名を登録しました。次にクラスを入力してください。")
+                    users[user_id]["register_status"] = "waiting_class"
+                    users[user_id]["name"] = text
+                    save_users(users)
+                    return "OK"
+                
+            elif register_status == "waiting_class":
+                if text != "A" and text != "B" and text != "C" and text != "D":
+                    reply_message(reply_token, "正しいクラスを入力してください。")
+                    return "OK"
+                else:
+                    reply_message(reply_token, "ユーザー情報の登録が完了しました。")
+                    users[user_id]["register_status"] = "registered"
+                    users[user_id]["class"] = text
+                    save_users(users)
+                    return "OK"
 
-            elif status == "waiting_class":
-                reply_message(reply_token, "ユーザー情報の登録が完了しました。")
-                users[user_id]["status"] = "registered"
-                users[user_id]["class"] = text
-                save_users(users)
-                return "OK"
+            elif register_status == "registered":
+                if service_status == "None":
+                    if text == "サービスを利用":
+                        reply_message(reply_token, "科目を選択してください。\n1. 国語\n2. 数学\n3. 英語")
+                        users[user_id]["service_status"] = "waiting_subject"
+                        save_users(users)
+                        return "OK"
 
+                    elif service_status == "waiting_subject":
+                        if text not in ["国語", "数学", "英語"]:
+                            reply_message(reply_token, "利用可能な科目を選択してください。")
+                            return "OK"
+                        else:
+                            reply_message(reply_token, text + "のプリントを送信します。")
+                            users[user_id]["service_status"] = "None"
+                            save_users(users)
+                            return "OK"
+                    
             else:
-                reply_message(reply_token, "hello, " + users[user_id]["name"] + ".\n You've already registered.")
+                reply_message(reply_token, "こんにちは、" + users[user_id]["name"] + "さん。\n サービスを利用する際は、画面下部の「サービスを利用」ボタンをタップしてください。")
                 return "OK"
 
 def reply_message(reply_token, text):
