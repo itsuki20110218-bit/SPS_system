fb9c8212850e95d6040b208ff0c9a15c6dc6fdae
Itsuki <itsuki20110218@icloud.com>
2026-01-17 12:32:32 +0900

    Update SPSsystem.py

--- SPSsystem.py
+++ SPSsystem.py
@@ -666,13 +666,12 @@ def reply_message(reply_token, text, show_cancel=False, show_class=False, show_p
     users= load_users()
     prints = load_prints()
     if show_print_numbers:
-        subjects = (
-        users[user_id].get("admin_current_subject")
-        if users[user_id]["mode"] == "admin"
-        else users[user_id].get("current_subject")
-        )
+        if users[user_id]["mode"] == "admin":
+            subject = users[user_id].get("admin_current_subject")
+        else:
+            subject = users[user_id].get("current_subject")
         page = users[user_id].get("print_page", 0)
-        all_numbers = list(prints.get(subjects, {}).keys())
+        all_numbers = list(prints.get(subject, {}).keys())
         page_numbers = get_print_numbers_by_page(all_numbers, page)
 
         for number in page_numbers:
