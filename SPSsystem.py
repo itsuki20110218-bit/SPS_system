a227b550bd7e3733e95770a3b47ee3dd68243170
Itsuki <itsuki20110218@icloud.com>
2026-04-15 16:34:45 +0900

    Update all_subjects to include more subjects

--- SPSsystem.py
+++ SPSsystem.py
@@ -14,7 +14,7 @@ PRINT_FILE = "prints.json"
 ADMIN_IDS = "admin_ids.json"
 
 classes = ["A", "B", "C", "D", "E", "F", "G", "H"]
-all_subjects = ["おためし", "数学"]
+all_subjects = ["国語", "数学", "理科", "社会", "英語"]
 
 def load_admin_ids():
     if not os.path.exists(ADMIN_IDS):
